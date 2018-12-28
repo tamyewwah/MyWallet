eee67fe9e775c9f9d209727df52fd78e7f07266a
tamyewwah <44257776+tamyewwah@users.noreply.github.com>
2018-12-29 03:01:26 +0800

    First

--- app/src/main/java/com/gmail/tamyewwah/mywallet/MainActivity.java
+++ app/src/main/java/com/gmail/tamyewwah/mywallet/MainActivity.java
@@ -1,13 +1,102 @@
 package com.gmail.tamyewwah.mywallet;
 
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer =findViewById(R.id.main_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.open,R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment selectedFragment = null;
+        switch (menuItem.getItemId())
+        {
+            case R.id.nav_transactionRecord:
+                selectedFragment = new TransactionRecordFragment();
+                break;
+            case R.id.nav_balance:
+                selectedFragment = new BalanceFragment();
+                break;
+            case R.id.nav_feedback:
+                selectedFragment = new FeedbackFragment();
+                break;
+            case R.id.nav_message:
+                selectedFragment = new MessageFragment();
+                break;
+            case R.id.nav_setting:
+                selectedFragment = new SettingFragment();
+                break;
+            case R.id.nav_help:
+                selectedFragment = new HelpFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch(item.getItemId())
+            {
+                case R.id.nav_payment:
+                    selectedFragment = new PaymentFragment();
+                    break;
+                case R.id.nav_transfer:
+                    selectedFragment = new TransferFragment();
+                    break;
+                case R.id.nav_MyWallet:
+                    selectedFragment = new MyWalletFragment();
+                    break;
+                case R.id.nav_promotion:
+                    selectedFragment = new PromotionFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
 }
