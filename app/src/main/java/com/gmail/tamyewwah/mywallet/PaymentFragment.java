a895712a41b0c3df22dacb9fc24489a51f3f1c59
tamyewwah <44257776+tamyewwah@users.noreply.github.com>
2019-01-03 02:35:43 +0800

    efficient keyboard/completed qrcode/ProgressBar

--- app/src/main/java/com/gmail/tamyewwah/mywallet/PaymentFragment.java
+++ app/src/main/java/com/gmail/tamyewwah/mywallet/PaymentFragment.java
@@ -1,39 +1,25 @@
 package com.gmail.tamyewwah.mywallet;
 
-import android.Manifest;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.net.sip.SipSession;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.zxing.Result;
 
-import org.w3c.dom.Text;
-
-import java.io.IOException;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -41,20 +27,32 @@ import static android.Manifest.permission_group.CAMERA;
 
 
 public class PaymentFragment extends Fragment implements ZXingScannerView.ResultHandler {
-    SurfaceView surfaceView;
+
     TextView textview;
     private static final int REQUEST_CAMERA = 1;
     private ZXingScannerView scannerView;
+    private LinearLayout qrCamera;
+    private  String userID;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_payment, container, false);
 
-        scannerView = new ZXingScannerView(getActivity());
-        getActivity().setContentView(scannerView);
-
+        MainActivity mainActivity =(MainActivity)getActivity();
+        userID = mainActivity.getUserID();
+        qrCamera = view.findViewById(R.id.qrcodecamera);
+        scannerView = new ZXingScannerView(getActivity().getApplicationContext());
+        scannerView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        ));
+        qrCamera.addView(scannerView);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkPermission()) {
                 Toast.makeText(getActivity(), "Permission is granted", Toast.LENGTH_LONG).show();
@@ -63,80 +61,16 @@ public class PaymentFragment extends Fragment implements ZXingScannerView.Result
             }
         }
 
-//        surfaceView = view.findViewById(R.id.cameraPreview);
-//        textview = view.findViewById(R.id.txtResult);
-//
-//        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(getActivity())
-//                .setBarcodeFormats(Barcode.QR_CODE).build();
-//
-//
-//
-//        final CameraSource cameraSource = new CameraSource.Builder(getActivity(),barcodeDetector)
-//                .setRequestedPreviewSize(640,480).build();
-//
-//        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-//            @Override
-//            public void surfaceCreated(SurfaceHolder holder) {
-//                if(ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(),Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED)
-//                {
-//                    return;
-//                }
-//                try
-//                {
-//
-//                    cameraSource.start(holder);
-//                }catch (Exception e)
-//                {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            @Override
-//            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-//
-//
-//            }
-//
-//            @Override
-//            public void surfaceDestroyed(SurfaceHolder holder) {
-//
-//                cameraSource.stop();
-//            }
-//        });
-//
-//         barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-//             @Override
-//             public void release() {
-//
-//             }
-//
-//             @Override
-//             public void receiveDetections(Detector.Detections<Barcode> detections) {
-//                 final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
-//                 if(qrcodes.size()!=0)
-//                 {
-//                     textview.post(new Runnable() {
-//                         @Override
-//                         public void run() {
-//                             Vibrator vibrator = (Vibrator) getActivity().getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-//                             vibrator.vibrate(1000);
-//                             textview.setText(qrcodes.valueAt(0).displayValue );
-//
-//                         }
-//                     });
-//                 }
-//             }
-//         });
         return view;
     }
 
     private boolean checkPermission() {
 
-        return (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
+        return (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
     }
 
     private void requestPermission() {
-        ActivityCompat.requestPermissions(getActivity(), new String[]{CAMERA}, REQUEST_CAMERA);
+        ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.CAMERA}, REQUEST_CAMERA);
     }
 
     @Override
@@ -184,8 +118,8 @@ public class PaymentFragment extends Fragment implements ZXingScannerView.Result
             {
                 if (scannerView==null)
                 {
-                    scannerView=new ZXingScannerView(getActivity());
-                    getActivity().setContentView(scannerView);
+                    scannerView = new ZXingScannerView(getActivity().getApplicationContext());
+                    qrCamera.addView(scannerView);
                 }
                 scannerView.setResultHandler(this);
                 scannerView.startCamera();
@@ -198,8 +132,8 @@ public class PaymentFragment extends Fragment implements ZXingScannerView.Result
     }
 
     @Override
-    public void onDestroy(){
-        super.onDestroy();
+    public void onPause(){
+        super.onPause();
         scannerView.stopCamera();
     }
 
@@ -216,6 +150,7 @@ public class PaymentFragment extends Fragment implements ZXingScannerView.Result
     @Override
     public void handleResult(Result result) {
         final String scanResult =result.getText();
+        String FinalResult;
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Scan Result");
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -224,11 +159,25 @@ public class PaymentFragment extends Fragment implements ZXingScannerView.Result
                 scannerView.resumeCameraPreview(PaymentFragment.this);
             }
         });
-        builder.setNeutralButton("Visit", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(scanResult));
-            }
-        });
+        if(scanResult.contains("http://")||scanResult.contains("www")||scanResult.contains(".com")) {
+            builder.setNeutralButton("Visit", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(scanResult));
+                    startActivity(intent);
+
+
+                }
+            });
+            FinalResult=scanResult;
+        }
+        else
+        {
+            FinalResult="Pay To :"+scanResult.substring(0,scanResult.indexOf("-"))+"\n"+"RM :"+scanResult.substring(scanResult.indexOf("-")+1,scanResult.length());
+        }
+        builder.setMessage(FinalResult);
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
