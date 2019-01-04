c9389bed99dc9a64e6a8b3868ed16485d53387ce
waizhenhao <44257736+waizhenhao@users.noreply.github.com>
2019-01-04 08:48:38 +0800

    Add one fragment and one java class

---
+++ app/src/main/java/com/gmail/tamyewwah/mywallet/AddCardDetails.java
@@ -0,0 +1,7 @@
+package com.gmail.tamyewwah.mywallet;
+
+import android.support.v4.app.Fragment;
+
+public class AddCardDetails extends Fragment {
+
+}
--- app/src/main/java/com/gmail/tamyewwah/mywallet/MyWalletFragment.java
+++ app/src/main/java/com/gmail/tamyewwah/mywallet/MyWalletFragment.java
@@ -11,6 +11,6 @@ public class MyWalletFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mywallet, container,false);
+        return inflater.inflate(R.layout.fragment_add_card, container,false);
     }
 }
